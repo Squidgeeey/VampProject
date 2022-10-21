eb95b0e0bb47789f443f743e764a9a385691d0e2
Astraeus122 <ryanvaningenkal@gmail.com>
2022-10-21 15:54:29 +1300

    camera

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    //let camera follow target
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform target;
+        public float lerpSpeed = 1.0f;
+
+        private Vector3 offset;
+
+        private Vector3 targetPos;
+
+        private Vector3 position;
+
+        private void Start()
+        {
+            if (target == null) return;
+
+            offset = transform.position - target.position;
+        }
+
+        private void Update()
+        {
+            position.x = 15.0f;
+            position.y = 0.0f;
+            position.z = 0.0f;
+            if (target == null) return;
+
+            targetPos = target.position + offset;
+            targetPos -= position;
+            transform.position = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
+        }
+
+    }
+}
