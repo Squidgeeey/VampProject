b79e612fecc0e16d15d0a9cdf504566a679abc78
Squidgeey <jayden.drube@gmail.com>
2022-10-28 12:35:27 +1300

    Umbrella holding "implemented"

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,10 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public bool isHoldingUmbrella = false;
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("LightTile")) // You can name this tag whatever you want, just make sure to tag all your death triggers with this
+        if (col.CompareTag("LightTile") && !(isHoldingUmbrella)) // You can name this tag whatever you want, just make sure to tag all your death triggers with this
         {
             //Reset game here:
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
@@ -26,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] public Sprite idle;
     [SerializeField] public Sprite pushRight;
     [SerializeField] public Sprite pushLeft;
+    [SerializeField] public int delay;
     void ChangeSprite(Sprite _NewSprite)
     {
         this.GetComponent<SpriteRenderer>().sprite = _NewSprite;
@@ -42,6 +43,11 @@ public class PlayerMovement : MonoBehaviour
 
         
 
+    }
+
+    void HoldUmbrellaStop()
+    {
+        isHoldingUmbrella = false;
     }
 
     void FixedUpdate()
@@ -71,6 +77,11 @@ public class PlayerMovement : MonoBehaviour
         }
         
         
+        if(Input.GetKey(KeyCode.Space))
+        {
+            isHoldingUmbrella = true;
+            Invoke("HoldUmbrellaStop", delay);
+        }
 
 
 
